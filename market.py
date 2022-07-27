29904bf2b64dedabef5af1a3eb5463f0baca8100
mfarzamalam <mfarzamalam@gmail.com>
2022-07-27 18:28:45 +0500

    added market page with some raw data for actions

--- market.py
+++ market.py
@@ -15,4 +15,15 @@ def user_profile(username):
 @app.route('/home')
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', page_name='home')
+
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id':1, 'name': 'Product Name 1', 'barcode':'asd213asdd2vggxz', 'price':201},
+        {'id':2, 'name': 'Product Name 2', 'barcode':'asd213asdd2vggxz', 'price':202},
+        {'id':3, 'name': 'Product Name 3', 'barcode':'asd213asdd2vggxz', 'price':203},
+        {'id':4, 'name': 'Product Name 4', 'barcode':'asd213asdd2vggxz', 'price':204},
+    ]
+    return render_template('market.html', page_name='market', items=items)
