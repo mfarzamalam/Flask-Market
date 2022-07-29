5d4b41841bedb601b022902c47a23c129f3b71b7
mfarzamalam <mfarzamalam@gmail.com>
2022-07-29 20:13:29 +0500

    restructure the whole app

---
+++ market/models.py
@@ -0,0 +1,12 @@
+from market import db
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=30), nullable=False, unique=True)
+    desc = db.Column(db.String(length=1024), nullable=False)
+
+    def __repr__(self):
+        return str(self.name)
---
+++ market/views.py
@@ -0,0 +1,25 @@
+from market import app
+from flask import render_template
+from market.models import Item
+
+
+@app.route('/starter')
+def hello_world():
+    return "Hello, World!!!"
+
+
+@app.route('/profile/<username>')
+def user_profile(username):
+    return f"Welcome {username}!"
+
+
+@app.route('/home')
+@app.route('/')
+def home_page():
+    return render_template('home.html', page_name='home')
+
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', page_name='market', items=items)
